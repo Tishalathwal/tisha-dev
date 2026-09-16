f6c6593651fd94cc69c69398ebc9946f832cd674
Tishalathwal <tishalathwal55@gmail.com>
2026-09-16 12:45:57 +0530

    Add request path logging for debugging

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ mongoose.set('bufferCommands', false);
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
